bff2dbfc5a25de228677c7715dacb33c5dc80fec
Cheesephu <rcpowerbest@gmail.com>
2022-12-06 21:49:21 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,21 @@ buttons.forEach(btn => {
 document.getElementById("contact-me").onclick = function() {
     window.location.href = "https://www.facebook.com/CheesePhu/";
 };
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
